c0ca835a8666d4913325fc070b494a61da8349c0
sumanhope <sumansthahope@gmail.com>
2023-09-29 20:30:58 +0545

    change icon and solved hosting errors

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
-import { Route, Routes } from 'react-router-dom';
-import './App.css';
-import Header from './Components/Header';
-import {Home, Rooms, Favorite, About, OurTeam, Contact, Signin, Register, Recover} from './Components/index';
-
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Header from "./Components/Header";
+import {
+  Home,
+  Rooms,
+  Favorite,
+  About,
+  OurTeam,
+  Contact,
+  Signin,
+  Register,
+  Recover,
+} from "./Components/index";
 
 const App = () => {
   return (
     <div>
       <Routes>
-      <Route exact path='/' element={<Home/>}/>
-      <Route path='/Rooms' element={<Rooms/>}/>
-      <Route path='/Favorite' element={<Favorite/>}/>
-      <Route path='/About' element={<About/>}/>
-      <Route path='/OurTeam' element={<OurTeam/>}/>
-      <Route path='/Contact' element={<Contact/>}/>
-      <Route path='/Signin' element={<Signin/>}/>
-      <Route path='/Register' element={<Register/>}/>
-      <Route path='/Recover' element={<Recover/>}/>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/Rooms" element={<Rooms />} />
+        <Route path="/Favorite" element={<Favorite />} />
+        <Route path="/About" element={<About />} />
+        <Route path="/OurTeam" element={<OurTeam />} />
+        <Route path="/Contact" element={<Contact />} />
+        <Route path="/Signin" element={<Signin />} />
+        <Route path="/Register" element={<Register />} />
+        <Route path="/Recover" element={<Recover />} />
       </Routes>
     </div>
   );
