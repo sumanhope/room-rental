2c8026e88070fd7815d04cc16dcfab0916df6f88
sumanhope <sumansthahope@gmail.com>
2023-10-03 14:10:09 +0545

    Header responsive

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,12 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { NavLink } from "react-router-dom";
 import Logo from "./Logo";
 import { GetUserInfo } from "../config/user-info";
 import { signOut } from "firebase/auth";
 import { auth } from "../config/firebase-config";
+import { AiOutlineMenu } from "react-icons/ai";
+import { AiOutlineClose } from "react-icons/ai";
 
-const Header = () => {
+const Nav = () => {
   const { username, isAuth } = GetUserInfo();
   const displayname = isAuth ? username : null;
   const signUserOut = async () => {
@@ -19,67 +21,82 @@ const Header = () => {
     }
   };
   // console.log(isAuth, username);
+
+  let Links = [
+    { name: "HOME", link: "/" },
+    { name: "ROOMS", link: "/Rooms" },
+    { name: "FAVORITE", link: "/Favorite" },
+    { name: "ABOUT", link: "/About" },
+    { name: "TEAM", link: "/OurTeam" },
+    { name: "CONTACT", link: "/Contact" },
+  ];
+  let [open, setOpen] = useState(false);
+
   return (
-    <nav className="sticky top-0 z-40">
-      <div
-        id="header"
-        className="h-20 py-12 px-[10%] shadow-xl flex items-center justify-between  top-0 bg-white overflow-hidden"
-      >
-        <Link to="/">
+    <nav className="shadow-md w-full fixed top-0 left-0">
+      <div className="md:flex items-center justify-between bg-white py-4 md:px-10 px-7">
+        <div>
           <Logo></Logo>
-        </Link>
-
-        <div className="flex gap-x-8 font-bold font-inter">
-          <NavLink to="/" className="nav-link">
-            Home
-          </NavLink>
-          <NavLink to="/Rooms" className="nav-link">
-            Rooms
-          </NavLink>
-          <NavLink to="/Favorite" className="nav-link">
-            Favorite
-          </NavLink>
-          <NavLink to="/About" className="nav-link">
-            About
-          </NavLink>
-          <NavLink to="/OurTeam" className="nav-link">
-            OurTeam
-          </NavLink>
-          <NavLink to="/Contact" className="nav-link">
-            Contact
-          </NavLink>
         </div>
 
-        <div className="login flex items-center gap-x-8 font-inter">
-          {isAuth ? (
-            // If the user is authenticated, display the username
-            <div className="font-bold">{displayname}</div>
-          ) : (
-            // If the user is not authenticated, display the "Sign in" link
-            <NavLink to="/Signin" className="nav-link font-bold">
-              Sign in
-            </NavLink>
-          )}
-          {isAuth ? (
-            <button
-              className="py-3 px-8 bg-customOrange font-bold text-white shadow-lg hover:-translate-y-1 duration-500"
-              onClick={signUserOut}
-            >
-              SignOut
-            </button>
-          ) : (
-            <Link
-              to="/Register"
-              id="register"
-              className="py-3 px-8 bg-customOrange font-bold text-white shadow-lg hover:-translate-y-1 duration-500"
-            >
-              Register
-            </Link>
-          )}
+        <div
+          onClick={() => setOpen(!open)}
+          className="text-3xl absolute right-8 top-6 cursor-pointer md:hidden"
+        >
+          {open ? <AiOutlineClose /> : <AiOutlineMenu />}
         </div>
+
+        <ul
+          className={`md:flex md:items-center md:pb-0 pb-12 absolute md:static bg-white md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in ${
+            open ? "top-20 " : "top-[-490px]"
+          }`}
+        >
+          {Links.map((link) => (
+            <li
+              key={link.name}
+              className=" nav-link md:ml-8 text-xl md:my-0 my-7"
+            >
+              <a href={link.link}>{link.name}</a>
+            </li>
+          ))}
+
+          <div className="login flex items-center md:ml-28 gap-x-8 font-inter cursor-pointer">
+            {isAuth ? (
+              // If the user is authenticated, display the username
+              <div className=" nav-link font-inter text-xl font-semibold uppercase">
+                {displayname}
+              </div>
+            ) : (
+              // If the user is not authenticated, display the "Sign in" link
+              <NavLink
+                to="/Signin"
+                className="nav-link font-inter text-xl font-semibold"
+              >
+                LOG IN
+              </NavLink>
+            )}
+
+            {isAuth ? (
+              <button
+                className="py-3 px-8 bg-customOrange font-inter uppercase text-x font-semibold text-white shadow-lg hover:-translate-y-1 duration-500"
+                onClick={signUserOut}
+              >
+                LOG OUT
+              </button>
+            ) : (
+              <Link
+                to="/Register"
+                id="register"
+                className="py-3 px-8 bg-customOrange font-inter uppercase text-x font-semibold text-white shadow-lg hover:-translate-y-1 duration-500"
+              >
+                Register
+              </Link>
+            )}
+          </div>
+        </ul>
       </div>
     </nav>
   );
 };
 
-export default Header;
+export default Nav;
