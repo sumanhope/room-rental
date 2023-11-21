bf95074ca27e8f3dabd41f940686c3f7ef2d3702
sumanhope <sumansthahope@gmail.com>
2023-11-21 11:56:30 +0545

    Changed About us content

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from "react";
 import Image from "../images/indoor.png";
 import { IoIosArrowForward } from "react-icons/io";
 export const About = () => {
@@ -6,22 +6,42 @@ export const About = () => {
     <div>
       <div className="flex flex-col font-inter justify-center items-center h-[calc(100vh-100px)] gap-x-40 sm:flex-row">
         <div className="text-black text-opacity-75 text-xs mt-10 p-4 border border-gray-100 rounded shadow-xl h-40vh sm:w-1/4">
-          <div id="Description" className="font-bold text-2xl  mt-5 flex font-inter ">
+          <div
+            id="Description"
+            className="font-bold text-2xl  mt-5 flex font-inter "
+          >
             About Us <IoIosArrowForward />
           </div>
           <div
-            id="Description" className="font-inter text-justify py-5"
+            id="Description"
+            className="font-inter text-justify py-5 text-xs"
           >
-            It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
+            Room Rental began as an idea among friends who were eager to deepen
+            their understanding of React and Firebase techonologies. Recognizing
+            the value of hands-on experience, we decide to create a unique
+            platform that would serve as a practical learning ground. This
+            virtual space may not be a real-world rental site, but it's a
+            testament to our dedication to continuous learning and pushing the
+            boundaries of what we can create.
+            <br />
+            Curious to see what Room Rental has to offer? Feel free to sign in
+            or register to experience the functionality firsthand. Navigate
+            through the user interface, interact with the features, and witness
+            the seamless integration of React and Firebase. This platform is a
+            living demonstration of our commitment to excellence and continuous
+            improvement.
+            <br />
+            Happy Coding!
           </div>
         </div>
         <div className="flex items-center justify-center">
-          <img className="shadow-xl w-full max-w-[500px] h-auto" src={Image} alt="room" />
+          <img
+            className="shadow-xl w-full max-w-[500px] h-auto"
+            src={Image}
+            alt="room"
+          />
         </div>
       </div>
     </div>
   );
-}
-
-
-
+};
